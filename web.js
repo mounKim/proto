d6c4930707ad266c2339fa439eb91ef69ab03682
San Kim <nasmik419@yonsei.ac.kr>
2023-10-01 00:30:12 +0900

    commit

--- web.js
+++ web.js
@@ -4,8 +4,8 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('abc-view-pug:server');
+var app = require('./app');
+var debug = require('debug')('proto:server');
 var http = require('http');
 
 /**
